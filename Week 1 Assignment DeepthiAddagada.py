e02e253f9313ba9a87980769016b3fd882177f9b
Deepthi21GIT <95394886+Deepthi21GIT@users.noreply.github.com>
2021-12-11 09:05:35 -0600

    Add files via upload

---
+++ Week 1 Assignment DeepthiAddagada.py
@@ -0,0 +1,57 @@
+# Week 1 Assignment Deepthi Addagada
+
+if __name__ == '__main__':
+         print ("\n")
+         # Greeting
+         print("Hello World! I wonder why that is always the default coding text to start with.")
+
+# Sum of two numbers
+print("\n")
+sum = int(20 + 30)
+print(sum)
+print("\n")
+
+# Subtract a number for the other number
+subtract = int(50-10)
+print(subtract)
+print("\n")
+
+# Multiply two numbers
+multiply = int(30 * 10)
+print(multiply)
+print("\n")
+
+# Divide between two numbers
+divide = int(600/200)
+print(divide)
+print("\n")
+
+# Concate two strings together
+x = " Hi, My name is Deepthi!"
+y = "This is my First class"
+z = x + y
+print (z)
+print ("\n")
+
+# Create a list of four items
+a = [2, 0.13, "true", 899, "This is a string"]
+print (a)
+print("\n")
+
+# Append an item to your list
+p = [20, 40, 60 ]
+p.append ("Eighty")
+print (p)
+print ("\n")
+
+# Create a tuple with four items
+fruits = ['apples', 'mango', 'berries', 'oranges']
+print (fruits)
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
